989a272ef93b80c7ea4c668e49aefac367e6b714
marci <barsonymarcell@gmail.com>
2022-05-31 23:59:44 +0200

    Added screen switching

--- qtile/config.py
+++ qtile/config.py
@@ -12,67 +12,127 @@ mod1 = "mod1"
 browser= "firefox"
 terminal = "alacritty"
 
+# Functions
+
+def window_to_prev_group(qtile):
+    if qtile.currentWindow is not None:
+        i = qtile.groups.index(qtile.currentGroup)
+        qtile.currentWindow.togroup(qtile.groups[i - 1].name)
+
+def window_to_next_group(qtile):
+    if qtile.currentWindow is not None:
+        i = qtile.groups.index(qtile.currentGroup)
+        qtile.currentWindow.togroup(qtile.groups[i + 1].name)
+
+def window_to_previous_screen(qtile):
+    i = qtile.screens.index(qtile.current_screen)
+    if i != 0:
+        group = qtile.screens[i - 1].group.name
+        qtile.current_window.togroup(group)
+
+def window_to_next_screen(qtile):
+    i = qtile.screens.index(qtile.current_screen)
+    if i + 1 != len(qtile.screens):
+        group = qtile.screens[i + 1].group.name
+        qtile.current_window.togroup(group)
+
+def switch_screens(qtile):
+    i = qtile.screens.index(qtile.current_screen)
+    group = qtile.screens[i - 1].group
+    qtile.current_screen.set_group(group)
+
 # Keys
 
 keys = [
 
-    # Terminal
-    Key([mod], "Return", lazy.spawn(terminal), desc="Launch terminal"),
+    # Qtile
+    Key([mod], "q", lazy.window.kill(), desc="Kill window"),
+    Key([mod, "control"], "q", lazy.shutdown(), desc="Shutdown"),
+    Key([mod, "control"], "r", lazy.reload_config(), desc="Reload"),
 
-    # Toggle layouts
-    Key([mod], "Tab", lazy.next_layout(), desc="Next layout"),
-    Key([mod, "shift"], "Tab", lazy.prev_layout(), desc="Prev layout"),
+    # Spawn
+    Key([mod], "Return", lazy.spawn(terminal), desc="Spawn terminal"),
+    Key([mod], "d", lazy.spawn("dmenu_run"), desc="Spawn dmenu"),
+    Key([mod], "r", lazy.spawncmd(), desc="Spawn command prompt"),
 
     # Full screen & Floating
     Key([mod], "f", lazy.window.toggle_fullscreen(), desc='Fullscreen'),
     Key([mod, "shift"], "f", lazy.window.toggle_floating(), desc='Floating'),
 
-    # Groups
-    Key([mod, "control"], "k", lazy.screen.next_group(), desc="Next group"),
-    Key([mod, "control"], "j", lazy.screen.prev_group(), desc="Prev group"),
-    Key([mod], "w", lazy.screen.toggle_group(), desc="Toggle last group"),
+    # Layouts
+    Key([mod], "Tab", lazy.next_layout(), desc="Next layout"),
+    Key([mod, "shift"], "Tab", lazy.prev_layout(), desc="Prev layout"),
 
-    # Kill & Reload
-    Key([mod], "q", lazy.window.kill(), desc="Kill window"),
-    Key([mod, "control"], "q", lazy.shutdown(), desc="Shutdown Qtile"),
-    Key([mod, "control"], "r", lazy.reload_config(), desc="Reload Qtile"),
+    # Groups
+    Key([mod, "mod1"], "j", lazy.screen.prev_group(), desc="Prev group"),
+    Key([mod, "mod1"], "k", lazy.screen.next_group(), desc="Next group"),
+    Key([mod], "b", lazy.screen.toggle_group(), desc="Toggle last group"),
 
     # Screen
-    Key([mod, "control"], "l", lazy.next_screen(), desc='Move focus to next monitor'),
-    Key([mod, "control"], "h", lazy.prev_screen(), desc='Move focus to prev monitor'),
-    #Key([mod], "t", lazy.to_screen(0)),
-    #Key([mod], "u", lazy.to_screen(1)),
-
-    # Spawn
-    Key([mod], "d", lazy.spawn("dmenu_run"), desc="Spawn dmenu"),
-    Key([mod], "r", lazy.spawncmd(), desc="Spawn command via prompt widget"),
-
-    # Test
-    #Key([mod, "control"], "t", lazy.group.next_window(), desc='Next window'),
+    Key([mod, "control"], "h", lazy.prev_screen(), desc='Focus prev monitor'),
+    Key([mod, "control"], "j",
+        lazy.function(window_to_previous_screen),
+        lazy.prev_screen(),
+        desc="Move & Focus prev screen"
+        ),
+    Key([mod, "control"], "k",
+        lazy.function(window_to_next_screen),
+        lazy.next_screen(),
+        desc="Move & Focus next screen"
+        ),
+    Key([mod, "control"], "l", lazy.next_screen(), desc='Focus next monitor'),
+    Key([mod], "w", lazy.to_screen(0)),
+    Key([mod], "e", lazy.to_screen(1)),
+    Key([mod], "r", lazy.to_screen(2)),
+    Key([mod], "t", lazy.function(switch_screens)),
 
     # Wallpaper
     #Key([mod], "h", lazy.screen.set_wallpaper(path, mode='fill/stretch'), desc="Set wallpaper"),
 
     # Windows - Focus
     Key([mod], "h", lazy.layout.left(), desc="Focus left"),
-    Key([mod], "l", lazy.layout.right(), desc="Focus right"),
     Key([mod], "j", lazy.layout.down(), desc="Focus down"),
     Key([mod], "k", lazy.layout.up(), desc="Focus up"),
+    Key([mod], "l", lazy.layout.right(), desc="Focus right"),
     Key([mod], "space", lazy.layout.next(), desc="Focus other window"),
 
     # Windows - Move
     Key([mod, "shift"], "h", lazy.layout.shuffle_left(), desc="Move left"),
-    Key([mod, "shift"], "l", lazy.layout.shuffle_right(), desc="Move right"),
     Key([mod, "shift"], "j", lazy.layout.shuffle_down(), desc="Move down"),
     Key([mod, "shift"], "k", lazy.layout.shuffle_up(), desc="Move up"),
+    Key([mod, "shift"], "l", lazy.layout.shuffle_right(), desc="Move right"),
 
     # Windows - Resize
-    Key([mod1], "h", lazy.layout.grow_left(), desc="Grow left"),
-    Key([mod1], "l", lazy.layout.grow_right(), desc="Grow right"),
-    Key([mod1], "j", lazy.layout.grow_down(), desc="Grow down"),
-    Key([mod1], "k", lazy.layout.grow_up(), desc="Grow up"),
-    Key([mod1], "n", lazy.layout.normalize(), desc="Reset sizes"),
-
+    Key([mod1], "h",
+        lazy.layout.grow_left(),
+        lazy.layout.shrink(),
+        lazy.layout.decrease_ratio(),
+        lazy.layout.add(),
+        desc="Grow left"
+        ),
+    Key([mod1], "j",
+        lazy.layout.grow_down(),
+        lazy.layout.shrink(),
+        lazy.layout.increase_nmaster(),
+        desc="Grow down"
+        ),
+    Key([mod1], "k",
+        lazy.layout.grow_up(),
+        lazy.layout.grow(),
+        lazy.layout.decrease_nmaster(),
+        desc="Grow up"
+        ),
+    Key([mod1], "l",
+        lazy.layout.grow_right(),
+        lazy.layout.grow(),
+        lazy.layout.increase_ratio(),
+        lazy.layout.delete(),
+        desc="Grow right"
+        ),
+    Key([mod1], "n",
+        lazy.layout.normalize(),
+        desc="Reset sizes"
+        ),
 
     # Toggle between split and unsplit sides of stack.
     # Split = all windows displayed
