e03f673b995d94507638383089ca51c98e8e98d8
marcellbarsony <barsonymarcell@protonmail.com>
2023-10-06 22:46:01 +0200

    Indent-Blankline v3.0

--- nvim/lua/user/plugins/indent-blankline.lua
+++ nvim/lua/user/plugins/indent-blankline.lua
@@ -1,36 +1,49 @@
 -- Indent blankline
 -- https://github.com/lukas-reineke/indent-blankline.nvim
 
+-- rainbow-deliometers.nvim integration
+-- https://github.com/lukas-reineke/indent-blankline.nvim#rainbow-delimitersnvim-integration
 local highlight = {
-    "RainbowRed",
-    "RainbowYellow",
-    "RainbowBlue",
-    "RainbowOrange",
-    "RainbowGreen",
-    "RainbowViolet",
-    "RainbowCyan",
-    -- "CursorColumn",
-    -- "Whitespace",
+  "RainbowRed",
+  "RainbowYellow",
+  "RainbowBlue",
+  "RainbowOrange",
+  "RainbowGreen",
+  "RainbowViolet",
+  "RainbowCyan",
 }
 
 local hooks = require "ibl.hooks"
--- create the highlight groups in the highlight setup hook, so they are reset
--- every time the colorscheme changes
+
 hooks.register(hooks.type.HIGHLIGHT_SETUP, function()
-    vim.api.nvim_set_hl(0, "RainbowRed", { fg = "#61AFEF" })
-    vim.api.nvim_set_hl(0, "RainbowYellow", { fg = "#E5C07B" })
-    vim.api.nvim_set_hl(0, "RainbowBlue", { fg = "#61AFEF" })
-    vim.api.nvim_set_hl(0, "RainbowOrange", { fg = "#D19A66" })
-    vim.api.nvim_set_hl(0, "RainbowGreen", { fg = "#98C379" })
-    vim.api.nvim_set_hl(0, "RainbowViolet", { fg = "#C678DD" })
-    vim.api.nvim_set_hl(0, "RainbowCyan", { fg = "#56B6C2" })
+  vim.api.nvim_set_hl(0, "RainbowRed", { fg = "#C678DD" })
+  vim.api.nvim_set_hl(0, "RainbowYellow", { fg = "#C678DD" })
+  vim.api.nvim_set_hl(0, "RainbowBlue", { fg = "#C678DD" })
+  vim.api.nvim_set_hl(0, "RainbowOrange", { fg = "#D19A66" })
+  vim.api.nvim_set_hl(0, "RainbowGreen", { fg = "#98C379" })
+  vim.api.nvim_set_hl(0, "RainbowViolet", { fg = "#C678DD" })
+  vim.api.nvim_set_hl(0, "RainbowCyan", { fg = "#56B6C2" })
 end)
 
-require("ibl").setup {
-    indent = { highlight = highlight, char = "" },
-    whitespace = {
-        highlight = highlight,
-        remove_blankline_trail = false,
-    },
-    scope = { enabled = true },
+vim.g.rainbow_delimiters = {
+  highlight = highlight
 }
+
+require("ibl").setup {
+  debounce = 200,
+  scope = {
+    highlight = highlight
+  },
+  --indent = { char = "|" },
+  --whitespace = {
+  --  highlight = {
+  --    "Whitespace",
+  --    "NonText"
+  --  }
+  --},
+}
+
+hooks.register(
+  hooks.type.SCOPE_HIGHLIGHT,
+  hooks.builtin.scope_highlight_from_extmark
+)
