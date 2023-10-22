63801586b39babc1a320d94afba1c1dc943f5cc9
marcellbarsony <barsonymarcell@protonmail.com>
2023-10-22 04:49:11 +0200

    Arkenfox preferences

--- firefox/preferences/user-overrides.js
+++ firefox/preferences/user-overrides.js
@@ -10,7 +10,8 @@ user_pref("privacy.userContext.newTabContainersOnLeftClick.enabled", true);
 // user_pref("network.http.referer.XOriginTrimmingPolicy", 0); // 1602
 
 /* EXTENSIONS */
-user_pref("extensions.pocket.enabled", false);
+// https://support.mozilla.org/en-US/kb/disable-or-re-enable-pocket-for-firefox
+user_pref("extensions.pocket.enabled", false); // 1702
 
 /* FIND BAR */
 user_pref("findbar.highlightAll", true);
@@ -22,8 +23,8 @@ user_pref("full-screen-api.warning.timeout", 0);
 /* LAYOUT */
 user_pref("layout.css.devPixelsPerPx", "0.9");
 
-/* MEDIA */
-user_pref("media.eme.enabled", true); // 2022
+/* MEDIA (DRM)*/
+user_pref("media.eme.enabled", true); // 5508
 
 /* PROMO */
 user_pref("browser.promo.focus.enabled", false);
@@ -34,15 +35,32 @@ user_pref("browser.vpn_promo.enabled", false);
 user_pref("privacy.resistFingerprinting.letterboxing", false); // 4504
 
 /* SEARCH */
+user_pref("browser.search.suggest.enabled", false);
+user_pref("browser.urlbar.accessibility.tabToSearch.announceResults", false)
 user_pref("browser.urlbar.placeholderName", "DuckDuckGo");
 user_pref("browser.urlbar.placeholderName.private", "DuckDuckGo");
-user_pref("browser.search.suggest.enabled", false);
+user_pref("browser.urlbar.suggest.addons", false);
+user_pref("browser.urlbar.suggest.bestmatch", false);
 user_pref("browser.urlbar.suggest.bookmark", false);
+user_pref("browser.urlbar.suggest.calculator", false);
+user_pref("browser.urlbar.suggest.clipboard", false);
+user_pref("browser.urlbar.suggest.engines", false);
 user_pref("browser.urlbar.suggest.history", false);
+user_pref("browser.urlbar.suggest.mdn", false);
 user_pref("browser.urlbar.suggest.openpage", false);
+user_pref("browser.urlbar.suggest.pocket", false);
 user_pref("browser.urlbar.suggest.quicksuggest.nonsponsored", false);
 user_pref("browser.urlbar.suggest.quicksuggest.sponsored", false);
+user_pref("browser.urlbar.suggest.remotetab", false);
 user_pref("browser.urlbar.suggest.topsites", false);
+user_pref("browser.urlbar.suggest.trending", false);
+user_pref("browser.urlbar.suggest.weather", false);
+user_pref("services.sync.prefs.sync.browser.urlbar.suggest.bookmark", false);
+user_pref("services.sync.prefs.sync.browser.urlbar.suggest.engines", false);
+user_pref("services.sync.prefs.sync.browser.urlbar.suggest.history", false);
+user_pref("services.sync.prefs.sync.browser.urlbar.suggest.openpage", false);
+user_pref("services.sync.prefs.sync.browser.urlbar.suggest.searches", false);
+user_pref("services.sync.prefs.sync.browser.urlbar.suggest.topsites", false);
 
 /* SHUTDOWN */
 user_pref("privacy.clearOnShutdown.history", false); // 2811
