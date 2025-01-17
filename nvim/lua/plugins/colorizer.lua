4305fb4727976784c1701f593eb67f74db14f178
marcellbarsony <barsonymarcell@protonmail.com>
2025-01-17 02:09:01 +0100

    Backing up changes

--- nvim/lua/plugins/colorizer.lua
+++ nvim/lua/plugins/colorizer.lua
@@ -1,27 +1,39 @@
 -- Colorizer
--- https://github.com/NvChad/nvim-colorizer.lua
+-- https://github.com/catgoose/nvim-colorizer.lua
 -- `:h colorizer`
 
 return {
   {
-    "NvChad/nvim-colorizer.lua",
+    "catgoose/nvim-colorizer.lua",
+    event = "BufReadPre",
     opts = {
-      -- https://github.com/NvChad/nvim-colorizer.lua#customization
-      filetypes = { "*" },
+      filetypes = { "*.css" },
+      lazy_load = true,
       user_default_options = {
-        RGB = true,                                      -- #RGB hex codes
-        RRGGBB = true,                                   -- #RRGGBB hex codes
         names = true,                                    -- "Name" codes like Blue or blue
+        names_opts = { -- options for mutating/filtering names.
+          lowercase = true, -- name:lower(), highlight `blue` and `red`
+          camelcase = true, -- name, highlight `Blue` and `Red`
+          uppercase = false, -- name:upper(), highlight `BLUE` and `RED`
+          strip_digits = false, -- ignore names with digits,
+          -- highlight `blue` and `red`, but not `blue3` and `red4`
+        },
+        names_custom = false,                            -- Custom names: table|function|false|nil
+        RGB = true,                                      -- #RGB hex codes
+        RGBA = true,                                     -- #RGBA hex codes
+        RRGGBB = true,                                   -- #RRGGBB hex codes
         RRGGBBAA = false,                                -- #RRGGBBAA hex codes
         AARRGGBB = false,                                -- 0xAARRGGBB hex codes
         rgb_fn = false,                                  -- CSS rgb() and rgba() functions
         hsl_fn = false,                                  -- CSS hsl() and hsla() functions
         css = false,                                     -- Enable all CSS features: rgb_fn, hsl_fn, names, RGB, RRGGBB
         css_fn = false,                                  -- Enable all CSS *functions*: rgb_fn, hsl_fn
-        mode = "virtualtext",                            -- Display mode: background, foreground, virtualtext
-        tailwind = "lsp",                                -- Enable tailwind colors: true / false / normal / lsp / both
+        tailwind = "lsp",                                -- Enable tailwind colors: true|false|normal|lsp|both
         sass = { enable = false, parsers = { "css" }, }, -- Enable sass colors
+        mode = "virtualtext",                            -- Display mode: background, foreground, virtualtext
         virtualtext = "■",
+        virtualtext_inline = false,
+        virtualtext_mode = "foreground",
         always_update = false,
       },
       buftypes = {},
